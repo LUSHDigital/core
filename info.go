893cba1f26017ce415cdfc8767a14bc6f20ac80d
Dan Richards <dan.richards@lush.co.uk>
2017-03-20 17:13:25 +0000

    Renamed helper.go to env.go

--- info.go
+++ info.go
@@ -4,11 +4,11 @@ import "github.com/LUSHDigital/microservice-core-golang/routing"
 
 // MicroserviceInfo - Represents information about this microservice.
 type MicroserviceInfo struct {
-	ServiceName    string         `json:"service_name"`
-	ServiceType    string         `json:"service_type"`
-	ServiceScope   string         `json:"service_scope"`
-	ServiceVersion string         `json:"service_version"`
-	Endpoints      []routes.Route `json:"endpoints"`
+	ServiceName    string          `json:"service_name"`
+	ServiceType    string          `json:"service_type"`
+	ServiceScope   string          `json:"service_scope"`
+	ServiceVersion string          `json:"service_version"`
+	Endpoints      []routing.Route `json:"endpoints"`
 }
 
 // GetMicroserviceInfo - Get the information about this microservice.
