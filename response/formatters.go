7b1b83259cf06b3884641aa152807bfe16eb335d
Dan Richards <dan.richards@lush.co.uk>
2017-05-02 17:14:34 +0100

    Resolved issue where the HTTP status code was not being set on the response.

--- response/formatters.go
+++ response/formatters.go
@@ -14,6 +14,9 @@ func JSONResponseFormatter(w http.ResponseWriter, response *MicroserviceReponse)
 	// Set the content type header.
 	w.Header().Set("Content-Type", "application/json")
 
+	// Set the status code.
+	w.WriteHeader(response.Code)
+
 	// Set the response.
 	json.NewEncoder(w).Encode(response)
 }
