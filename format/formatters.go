f2dfe638279eec3498957cc4c49831961afc1cb3
Benjamin Cable <ben.cable@lush.co.uk>
2017-07-25 14:45:56 +0100

    added format package, corrected tests

--- format/formatters.go
+++ format/formatters.go
@@ -1,8 +1,10 @@
-package response
+package format
 
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/LUSHDigital/microservice-core-golang/response"
 )
 
 // JSONResponseFormatter - Format a microservice response as JSON.
@@ -10,7 +12,7 @@ import (
 // Params:
 //     w http.ResponseWriter - The HTTP response writer.
 //     response *Response - The microservice response object.
-func JSONResponseFormatter(w http.ResponseWriter, response *Response) {
+func JSONResponseFormatter(w http.ResponseWriter, response *response.Response) {
 	// Set the content type header.
 	w.Header().Set("Content-Type", "application/json")
 
