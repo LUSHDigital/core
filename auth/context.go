0c27c19c479f85a3b739a8535cdd088d1b3e0859
nicklanng <nick.lanng@lush.co.uk>
2018-10-10 11:17:40 +0100

    Add missing doc comments

--- auth/context.go
+++ auth/context.go
@@ -12,10 +12,12 @@ const (
 	requestIDKey
 )
 
+// ContextWithConsumer takes a context and a service consumer and returns a new context with the consumer embedded.
 func ContextWithConsumer(parent context.Context, consumer Consumer) context.Context {
 	return context.WithValue(parent, consumerKey, consumer)
 }
 
+// ConsumerFromContext extracts the consumer from the supplied context.
 func ConsumerFromContext(c context.Context) Consumer {
 	return c.Value(consumerKey).(Consumer)
 }
