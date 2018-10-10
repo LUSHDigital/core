5ad263d76993e567c5985ecc99fc3f606919d1d6
nicklanng <nick.lanng@lush.co.uk>
2018-10-10 11:17:40 +0100

    Add missing doc comments

--- auth/grants.go
+++ auth/grants.go
@@ -7,8 +7,10 @@ import (
 
 const errorMissingRequiredGrants = "missing required grants"
 
+// Grant is a type alised string to represent permissions grants.
 type Grant string
 
+// HandlerGrants is an HTTP handler to check that the consumer in the request context has the required grants.
 func HandlerGrants(grants []Grant, next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		consumer := ConsumerFromContext(r.Context())
