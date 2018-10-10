b002afa33f0f597dbe2ed5ae426caed154d4b438
nicklanng <nick.lanng@lush.co.uk>
2018-10-10 14:12:11 +0100

    Rename grant checking function to better reflect the need for only one of the supplied grants

--- auth/consumer.go
+++ auth/consumer.go
@@ -6,8 +6,8 @@ type Consumer struct {
 	Grants []Grant `json:"grants"`
 }
 
-// HasGrants checks if a consumer possess any of a given set of grants
-func (c *Consumer) HasGrants(grants ...Grant) bool {
+// HasAnyGrant checks if a consumer possess any of a given set of grants
+func (c *Consumer) HasAnyGrant(grants ...Grant) bool {
 	for _, grant := range grants {
 		for _, g := range c.Grants {
 			if grant == g {
