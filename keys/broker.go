fed545c51034a832b9a273ebec1546f62dbb1682
zeeraw <zee@vall.in>
2018-11-23 16:36:28 +0100

    feat: implement a keys broker for rsa public keys

---
+++ keys/broker.go
@@ -0,0 +1,118 @@
+package keys
+
+import (
+	"context"
+	"crypto/rsa"
+	"fmt"
+	"log"
+	"math/big"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Renewer represents behaviour for marking a broker for renewal
+type Renewer interface {
+	Renew()
+}
+
+// RSAPublicKeyCopier represents behaviour for distributing copies of public keys
+type RSAPublicKeyCopier interface {
+	Copy() rsa.PublicKey
+}
+
+// RSAPublicKeyCopierRenewer represents the combination of a Copier and Renewer interface
+type RSAPublicKeyCopierRenewer interface {
+	RSAPublicKeyCopier
+	Renewer
+}
+
+var (
+	defaultRSA = &rsa.PublicKey{E: 0, N: big.NewInt(0)}
+)
+
+// BrokerRSAPublicKey will broker a public key from a source on an interval
+func BrokerRSAPublicKey(ctx context.Context, source Source, tick time.Duration) (*RSAPublicKeyBroker, func()) {
+	broker := &RSAPublicKeyBroker{
+		source:    source,
+		ticker:    time.NewTicker(tick),
+		key:       defaultRSA,
+		renew:     make(chan struct{}, 1),
+		cancelled: make(chan struct{}, 1),
+	}
+
+	// Make sure the broker is marked for renewal immediately
+	broker.Renew()
+	// Begin the key renewal
+	go broker.run(ctx)
+
+	// Return the broker together with a separate cancel function
+	// We do this to ensure cancellation is handeled correctly
+	return broker, broker.cancel
+}
+
+// RSAPublicKeyBroker defines the implementation for brokering an RSA public key
+type RSAPublicKeyBroker struct {
+	source    Source
+	ticker    *time.Ticker
+	key       *rsa.PublicKey
+	renew     chan struct{}
+	cancelled chan struct{}
+}
+
+// Copy returns a shallow copy o the RSA public key
+func (b *RSAPublicKeyBroker) Copy() rsa.PublicKey {
+	return *b.key
+}
+
+// Renew will inform the broker to force renewal of the key
+func (b *RSAPublicKeyBroker) Renew() {
+	select {
+	// Return early if the cancelled channel is already closed
+	case <-b.cancelled:
+		return
+	case b.renew <- struct{}{}:
+	// Exit if we can't send or receive on any channels
+	default:
+	}
+}
+
+// Close stops the ticker and releases resources
+func (b *RSAPublicKeyBroker) cancel() {
+	// Close the cancelled channel first to stop all select switches
+	close(b.cancelled)
+	b.ticker.Stop()
+	close(b.renew)
+}
+
+// Run will periodically try and the public key
+func (b *RSAPublicKeyBroker) run(ctx context.Context) {
+	for {
+		select {
+		case <-b.cancelled:
+			return
+		case <-b.ticker.C:
+			select {
+			case <-b.renew:
+				if err := b.get(ctx); err != nil {
+					log.Printf("RSA broker interval error: %v\n", err)
+				}
+			default:
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (b *RSAPublicKeyBroker) get(ctx context.Context) error {
+	bts, err := b.source.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot get key: %v", err)
+	}
+	b.key, err = jwt.ParseRSAPublicKeyFromPEM(bts)
+	if err != nil {
+		return fmt.Errorf("cannot parse key: %v", err)
+	}
+	return nil
+}
--- keys/sources.go
+++ keys/sources.go
@@ -1,19 +1,27 @@
 package keys
 
+import (
+	"os"
+)
+
 var (
-	// StagingTokenPublicKeySource represents the url to the JWT public key for the staging environment
-	StagingTokenPublicKeySource = HTTPSource("https://api-gateway-staging.platformserviceaccount.com/token/public")
+	// JWTPublicKeyEnvStringSource represents the source of an RSA public key as a string
+	JWTPublicKeyEnvStringSource = StringSource(os.Getenv("JWT_PUBLIC_KEY"))
 
-	// ProductionTokenPublicKeySource represents the url to the JWT public key for the production environment
-	ProductionTokenPublicKeySource = HTTPSource("https://api-gateway.platformserviceaccount.com/token/public")
+	// JWTPublicKeyEnvHTTPSource represents the source of an RSA public key at a HTTP GET destination
+	JWTPublicKeyEnvHTTPSource = HTTPSource(os.Getenv("JWT_PUBLIC_KEY_URL"))
 
-	// StagingTokenPublicKeySources represents all staging token public key sources
-	StagingTokenPublicKeySources = Sources{
-		StagingTokenPublicKeySource,
-	}
+	// JWTPublicKeyEnvFileSource represents the source of an RSA public key on disk
+	JWTPublicKeyEnvFileSource = FileSource(os.Getenv("JWT_PUBLIC_KEY_PATH"))
 
-	// ProductionTokenPublicKeySources represents all production token public key sources
-	ProductionTokenPublicKeySources = Sources{
-		ProductionTokenPublicKeySource,
+	// JWTPublicKeyDefaultFileSource represents the source of an RSA public key on disk
+	JWTPublicKeyDefaultFileSource = FileSource("/usr/local/var/jwt.pub")
+
+	// JWTPublicKeySources represents a chain of sources for JWT Public Keys
+	JWTPublicKeySources = Sources{
+		JWTPublicKeyEnvStringSource,
+		JWTPublicKeyEnvFileSource,
+		JWTPublicKeyEnvHTTPSource,
+		JWTPublicKeyDefaultFileSource,
 	}
 )
