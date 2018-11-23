370f41dabc8d4f83fbcc7d114a4ba5e2649e9be0
zeeraw <zee@vall.in>
2018-11-23 16:36:28 +0100

    feat: implement a keys broker for rsa public keys

--- keys/source.go
+++ keys/source.go
@@ -5,20 +5,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-// Source represents one key source
+// Source represents one or a chain of sources
 type Source interface {
-	GetKey(ctx context.Context) ([]byte, error)
+	Get(ctx context.Context) ([]byte, error)
 }
 
-// Sources represents multiple key sources
+// Sources defines a chain of sources
 type Sources []Source
 
-// GetKey iterates sources and returns the first successful response
-func (sources Sources) GetKey(ctx context.Context) ([]byte, error) {
+// Get iterates sources and returns the first successfully resolved
+func (sources Sources) Get(ctx context.Context) ([]byte, error) {
 	for _, source := range sources {
-		bts, err := source.GetKey(ctx)
+		bts, err := source.Get(ctx)
 		if err == nil {
 			return bts, nil
 		}
@@ -26,14 +27,17 @@ func (sources Sources) GetKey(ctx context.Context) ([]byte, error) {
 	return nil, ErrGetKeySource{"no sources could be resolved"}
 }
 
-// HTTPSource is a key with a URL source over HTTP
+// HTTPSource defines a source with a URL to resolve over HTTP
 type HTTPSource string
 
-// GetKey retrieves the key from the source over HTTP
-func (source HTTPSource) GetKey(ctx context.Context) ([]byte, error) {
+// Get retrieves data from the URL over HTTP
+func (source HTTPSource) Get(ctx context.Context) ([]byte, error) {
+	if source == "" {
+		return nil, ErrGetKeySource{"url cannot be empty"}
+	}
 	req, err := http.NewRequest(http.MethodGet, string(source), nil)
 	if err != nil {
-		return nil, ErrGetKeySource{}
+		return nil, ErrGetKeySource{err}
 	}
 	req = req.WithContext(ctx)
 	client := &http.Client{}
@@ -50,4 +54,31 @@ func (source HTTPSource) GetKey(ctx context.Context) ([]byte, error) {
 		return nil, ErrReadResponse{err}
 	}
 	return body, nil
+}
+
+// FileSource defines a path to a file on disk
+type FileSource string
+
+// Get retrieves data from the path to a file on disk
+func (source FileSource) Get(ctx context.Context) ([]byte, error) {
+	if source == "" {
+		return nil, ErrGetKeySource{"file path cannot be empty"}
+	}
+	f, err := os.Open(string(source))
+	if err != nil {
+		return nil, ErrGetKeySource{err}
+	}
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, ErrReadResponse{err}
+	}
+	return content, nil
+}
+
+// StringSource defines the source as a string
+type StringSource string
+
+// Get converts the string to a byte slice
+func (source StringSource) Get(ctx context.Context) ([]byte, error) {
+	return []byte(source), nil
 }
