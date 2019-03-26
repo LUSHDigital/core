238964970299a09f4841a3948331a999ea4cf4f2
Benjamin Cable <cable.benjamin@protonmail.com>
2019-03-26 16:45:27 +0000

    improved docs in service.go

--- service.go
+++ service.go
@@ -4,14 +4,14 @@ import (
 	"github.com/LUSHDigital/core/env"
 )
 
-// Service represents
+// Service represents the minimal information required to define a working micro-service.
 type Service struct {
 	Name    string `json:"service_name"`
 	Type    string `json:"service_type"`
 	Version string `json:"service_version"`
 }
 
-// GetService derives a service from the environment
+// GetService derives a service from the environment.
 func GetService() *Service {
 	return &Service{
 		Name:    env.MustGet("SERVICE_NAME"),
