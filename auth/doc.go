1d3aba2c02b4c75fd937aab463cc84505807a4c3
zeeraw <zee@vall.in>
2019-03-26 18:21:33 +0100

    docs: add some documentation to the auth package

---
+++ auth/doc.go
@@ -0,0 +1,3 @@
+// Package auth provides functions for services to issue and sign api consumer tokens.
+// It contains several middlewares for HTTP and GRPC to aid streamlining the authentication process.
+package auth
