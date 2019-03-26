4b2483b0e93dd9f91bb5a948dc1aa82c022da2f3
zeeraw <zee@vall.in>
2019-03-26 17:35:22 +0100

    feat: provide service struct for deriving information about the currently running service

---
+++ service.go
@@ -0,0 +1,21 @@
+package microservicecore
+
+import (
+	"github.com/LUSHDigital/core/env"
+)
+
+// Service represents
+type Service struct {
+	Name    string `json:"service_name"`
+	Type    string `json:"service_type"`
+	Version string `json:"service_version"`
+}
+
+// GetService derives a service from the environment
+func GetService() *Service {
+	return &Service{
+		Name:    env.MustGet("SERVICE_NAME"),
+		Type:    env.MustGet("SERVICE_TYPE"),
+		Version: env.MustGet("SERVICE_VERSION"),
+	}
+}
