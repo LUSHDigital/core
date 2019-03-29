e3e6cc0055c4521ebb23fc74ae3c40d2dacca011
Benjamin Cable <cable.benjamin@protonmail.com>
2019-03-29 15:35:30 +0000

    remove the transport package

--- transport/models/consumer.go
+++
@@ -1,7 +0,0 @@
-// Package models defines common data types used with the transport package.
-package models
-
-// Consumer - Holds information about an API consumer.
-type Consumer struct {
-	Tokens []*Token `json:"tokens,omitempty"` // The API consumers current access token.
-}
