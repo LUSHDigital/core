a46c8fab61c395990c5c1b811a50fac5876afa9e
zeeraw <zee@vall.in>
2019-04-01 12:20:01 +0200

    test: fix the grpc test and move to grpc test file

--- pagination/grpc_test.go
+++ pagination/grpc_test.go
@@ -1,32 +1,100 @@
 package pagination_test
 
 import (
-	"log"
+	"context"
+	"errors"
 	"net"
+	"testing"
 
 	"github.com/LUSHDigital/core/pagination"
 
+	greeter "github.com/LUSHDigital/core/pagination/internal/greeter"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
-func ExampleStreamServerInterceptor() {
-	srv := grpc.NewServer(
-		grpc.StreamInterceptor(pagination.StreamServerInterceptor),
-	)
-	l, err := net.Listen("tpc", ":50051")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Fatalln(srv.Serve(l))
+func TestInterceptServerRequest(t *testing.T) {
+	t.Run("valid incoming context", func(t *testing.T) {
+		ctx := context.Background()
+		md := metadata.New(map[string]string{
+			"per_page": "10",
+			"page":     "1",
+		})
+		ctx = metadata.NewIncomingContext(ctx, md)
+		req, err := pagination.InterceptServerRequest(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := pagination.Request{
+			PerPage: 10,
+			Page:    1,
+		}
+		equals(t, expected, req)
+	})
+	t.Run("invalid per page incoming context", func(t *testing.T) {
+		ctx := context.Background()
+		md := metadata.New(map[string]string{
+			"per_page": "abc",
+			"page":     "1",
+		})
+		ctx = metadata.NewIncomingContext(ctx, md)
+		_, err := pagination.InterceptServerRequest(ctx)
+		if err == nil {
+			t.Fatal("expected an error but got none")
+		}
+
+	})
+	t.Run("invalid page incoming context", func(t *testing.T) {
+		ctx := context.Background()
+		md := metadata.New(map[string]string{
+			"per_page": "10",
+			"page":     "abc",
+		})
+		ctx = metadata.NewIncomingContext(ctx, md)
+		_, err := pagination.InterceptServerRequest(ctx)
+		if err == nil {
+			t.Fatal("expected an error but got none")
+		}
+	})
 }
 
-func ExampleUnaryServerInterceptor() {
-	srv := grpc.NewServer(
+func TestGRPCInterceptor(t *testing.T) {
+	server := grpc.NewServer(
 		grpc.UnaryInterceptor(pagination.UnaryServerInterceptor),
+		grpc.StreamInterceptor(pagination.StreamServerInterceptor),
 	)
-	l, err := net.Listen("tpc", ":50051")
+	greeter.RegisterGreeterServer(server, &GreeterServer{})
+	listener, err := net.Listen("tcp", "")
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
-	log.Fatalln(srv.Serve(l))
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			panic(err)
+		}
+	}()
+	conn, err := grpc.Dial(listener.Addr().String(), grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	client := greeter.NewGreeterClient(conn)
+	md := metadata.New(map[string]string{
+		"per_page": "10",
+		"page":     "1",
+	})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	if _, err := client.SayHello(ctx, &greeter.Empty{}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+type GreeterServer struct{}
+
+func (*GreeterServer) SayHello(ctx context.Context, _ *greeter.Empty) (*greeter.Empty, error) {
+	req := pagination.RequestFromContext(ctx)
+	if req.Page != 1 || req.PerPage != 10 {
+		return &greeter.Empty{}, errors.New("failed to intercept")
+	}
+	return &greeter.Empty{}, nil
 }
