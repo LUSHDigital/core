36b285b9eab7b99555ecbb43005d119e3d2cb908
zeeraw <zee@vall.in>
2019-04-03 16:12:21 +0200

    docs: provide example service

---
+++ examples/service/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/LUSHDigital/core"
+	"github.com/LUSHDigital/core/workers/httpsrv"
+	"github.com/LUSHDigital/core/workers/keybroker"
+	"github.com/LUSHDigital/core/workers/metricsrv"
+)
+
+var (
+	service = &core.Service{
+		Name:    "example",
+		Type:    "service",
+		Version: "1.0.0",
+	}
+)
+
+func main() {
+	ctx := context.Background()
+
+	workers := []core.ServiceWorker{}
+
+	httphandler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("hello world"))
+	})
+	httpserver := &http.Server{
+		ReadTimeout: 10 * time.Second,
+	}
+
+	workers = append(workers, httpsrv.New(httphandler, httpserver))
+	workers = append(workers, metricsrv.New())
+	workers = append(workers, keybroker.NewRSA())
+
+	service.StartWorkers(ctx, workers...)
+}
