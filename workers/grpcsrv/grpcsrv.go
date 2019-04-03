bb39f1723a6f83f12cf4d48ab2db1620b59dc524
zeeraw <zee@vall.in>
2019-04-03 13:09:37 +0200

    feat: provide a grpc server service worker

---
+++ workers/grpcsrv/grpcsrv.go
@@ -0,0 +1,48 @@
+// Package grpcsrv provides a default set of configuration for hosting a grpc server in a service.
+package grpcsrv
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"time"
+
+	"github.com/LUSHDigital/core/workers/internal/portfmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+const (
+	// Port is the default gRPC port used in examples.
+	Port = 50051
+)
+
+// New sets up a new grpc server.
+func New(options ...grpc.ServerOption) *Server {
+	return &Server{grpc.NewServer(options...), Port, time.Now}
+}
+
+// Server represents a collection of functions for starting and running an RPC server.
+type Server struct {
+	Connection *grpc.Server
+	Port       int
+	Now        func() time.Time
+}
+
+// Run will start the gRPC server and listen for requests.
+func (gs *Server) Run(ctx context.Context, out io.Writer) error {
+	port := portfmt.Port(gs.Port)
+	lis, err := net.Listen("tcp", port.String())
+	if err != nil {
+		return err
+	}
+
+	hsrv := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(gs.Connection, hsrv)
+
+	gs.Port = lis.Addr().(*net.TCPAddr).Port
+	fmt.Fprintf(out, "serving grpc on 0.0.0.0:%d", gs.Port)
+	return gs.Connection.Serve(lis)
+}
