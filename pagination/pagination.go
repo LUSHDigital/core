0046a50c619fc2562462f0ecabb1b31c846e583e
zeeraw <zee@vall.in>
2019-08-09 10:55:28 +0200

    feat: provide method for deriving grpc metadata from a pagination response

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -1,6 +1,11 @@
 package pagination
 
-import "math"
+import (
+	"fmt"
+	"math"
+
+	"google.golang.org/grpc/metadata"
+)
 
 // Request derives the requested pagination data given by the client.
 type Request struct {
@@ -21,6 +26,24 @@ type Response struct {
 	CurrentPage uint64  `json:"current_page"` // The current page number.
 	NextPage    *uint64 `json:"next_page"`    // The number of the next page (if possible).
 	PrevPage    *uint64 `json:"prev_page"`    // The number of the previous page (if possible).
+}
+
+// Metadata returns gRPC metadata for a pagination response.
+func (r Response) Metadata() metadata.MD {
+	md := metadata.New(map[string]string{
+		"per_page":     fmt.Sprintf("%d", r.PerPage),
+		"offset":       fmt.Sprintf("%d", r.Offset),
+		"total":        fmt.Sprintf("%d", r.Total),
+		"last_page":    fmt.Sprintf("%d", r.LastPage),
+		"current_page": fmt.Sprintf("%d", r.CurrentPage),
+	})
+	if r.NextPage != nil {
+		md.Set("next_page", fmt.Sprintf("%d", *r.NextPage))
+	}
+	if r.PrevPage != nil {
+		md.Set("prev_age", fmt.Sprintf("%d", *r.PrevPage))
+	}
+	return md
 }
 
 // MakeResponse returns a new Response with the provided
