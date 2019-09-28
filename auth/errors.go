826f02ee55d021dd77efe62edf3818e4efa51f0a
zeeraw <zee@vall.in>
2019-09-28 13:02:54 +0200

    fix: lint errors

--- auth/errors.go
+++ auth/errors.go
@@ -8,7 +8,7 @@ var (
 	// ErrNotRSAPrivateKey happens when the key is not a valid RSA private key.
 	ErrNotRSAPrivateKey = errors.New("invalid key: must be a valid RSA private key")
 	// ErrNotPrivateKey happens when the key is neither an RSA or ECDSA private key.
-	ErrNotPrivateKey = errors.New("invalid key: must be either an RSA or ECDSA private key.")
+	ErrNotPrivateKey = errors.New("invalid key: must be either an RSA or ECDSA private key")
 	// ErrNotPublicKey happens when the key is neither an RSA or ECDSA public key.
-	ErrNotPublicKey = errors.New("invalid key: must be either an RSA or ECDSA public key.")
+	ErrNotPublicKey = errors.New("invalid key: must be either an RSA or ECDSA public key")
 )
