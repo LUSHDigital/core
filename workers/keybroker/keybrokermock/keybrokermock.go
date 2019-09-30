92487f1f7803386c285859aae618c8a26f746289
zeeraw <zee@vall.in>
2019-09-30 13:19:20 +0200

    feat: add mock for ECDSA public key broker

--- workers/keybroker/keybrokermock/keybrokermock.go
+++ workers/keybroker/keybrokermock/keybrokermock.go
@@ -1,9 +1,12 @@
 // Package keybrokermock implements no-op mocks for the keys package
 package keybrokermock
 
-import "crypto/rsa"
+import (
+	"crypto/ecdsa"
+	"crypto/rsa"
+)
 
-// MockRSAPublicKey resolves any source and returns a mocked RSAPublicKeyCopier and Renewer
+// MockRSAPublicKey resolves any source and returns a mocked RSAPublicKey Copier and Renewer
 func MockRSAPublicKey(key *rsa.PublicKey) *RSAPublicKeyMock {
 	return &RSAPublicKeyMock{
 		key: key,
@@ -27,5 +30,32 @@ func (b *RSAPublicKeyMock) Renew() {
 
 // Close is a no-op
 func (b *RSAPublicKeyMock) Close() {
+	// no-op
+}
+
+// MockECDSAPublicKey resolves any source and returns a mocked ECDSAPublicKey Copier and Renewer
+func MockECDSAPublicKey(key *ecdsa.PublicKey) *ECDSAPublicKeyMock {
+	return &ECDSAPublicKeyMock{
+		key: key,
+	}
+}
+
+// ECDSAPublicKeyMock defines the implementation for brokering an ECDSA public key during testing
+type ECDSAPublicKeyMock struct {
+	key *ecdsa.PublicKey
+}
+
+// Copy returns a shallow copy o the ECDSA public key
+func (b *ECDSAPublicKeyMock) Copy() ecdsa.PublicKey {
+	return *b.key
+}
+
+// Renew is a no-op
+func (b *ECDSAPublicKeyMock) Renew() {
+	// no-op
+}
+
+// Close is a no-op
+func (b *ECDSAPublicKeyMock) Close() {
 	// no-op
 }
