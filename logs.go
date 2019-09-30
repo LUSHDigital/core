195c737a7dc9ffbf322ec5abc05301bc08986e74
zeeraw <zee@vall.in>
2019-09-30 11:30:44 +0200

    feat: deprecate setup-logs

--- logs.go
+++ logs.go
@@ -3,6 +3,8 @@ package core
 import "log"
 
 // SetupLogs will set up the appropriate log flags.
+// DEPRECATED: Import github.com/LUSHDigital/core-lush/lushlogs as a side-effect.
 func SetupLogs() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.Println("DEPRECATED: import github.com/LUSHDigital/core-lush/lushlogs as a side-effect.")
 }
