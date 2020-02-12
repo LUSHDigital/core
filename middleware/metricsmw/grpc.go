22a30948e4f8bfaeafb63ed423ef254fd08da568
Benjamin Cable <cable.benjamin@protonmail.com>
2020-02-12 23:31:45 +0000

    Rename import alias to avoid snake case

--- middleware/metricsmw/grpc.go
+++ middleware/metricsmw/grpc.go
@@ -1,22 +1,22 @@
 package metricsmw
 
 import (
-	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	grpcprometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 )
 
 // DefaultServerMetrics is the default instance of ServerMetrics. It is
 // intended to be used in conjunction the default Prometheus metrics
 // registry.
-var DefaultServerMetrics = grpc_prometheus.DefaultServerMetrics
+var DefaultServerMetrics = grpcprometheus.DefaultServerMetrics
 
 // StreamServerInterceptor is a gRPC server-side interceptor that provides Prometheus monitoring for Streaming RPCs.
-var StreamServerInterceptor = grpc_prometheus.StreamServerInterceptor
+var StreamServerInterceptor = grpcprometheus.StreamServerInterceptor
 
 // UnaryServerInterceptor is a gRPC server-side interceptor that provides Prometheus monitoring for Unary RPCs.
-var UnaryServerInterceptor = grpc_prometheus.UnaryServerInterceptor
+var UnaryServerInterceptor = grpcprometheus.UnaryServerInterceptor
 
 // StreamClientInterceptor is a gRPC client-side interceptor that provides Prometheus monitoring for Streaming RPCs.
-var StreamClientInterceptor = grpc_prometheus.StreamClientInterceptor
+var StreamClientInterceptor = grpcprometheus.StreamClientInterceptor
 
 // UnaryClientInterceptor is a gRPC client-side interceptor that provides Prometheus monitoring for Unary RPCs.
-var UnaryClientInterceptor = grpc_prometheus.UnaryClientInterceptor
+var UnaryClientInterceptor = grpcprometheus.UnaryClientInterceptor
